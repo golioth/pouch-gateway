e127a9d99ddfbba5fe5e62d9e94e259fade8444c
Sam Friedman <sam@golioth.io>
2025-10-21 16:46:52 -0400

    bt: Implement application level acknowledgements
    This updates the GATT transport to use application level
    acknowledgements on top of unacknowledged GATT writes and
    notifications. This significantly increases throughput and
    matches the work done in the pouch repository.
    
    Signed-off-by: Sam Friedman <sam@golioth.io>

--- include/pouch_gateway/types.h
+++ include/pouch_gateway/types.h
@@ -23,6 +23,13 @@ enum pouch_gateway_gatt_attr
     POUCH_GATEWAY_GATT_ATTRS,
 };
 
+enum server_cert_next_state
+{
+    SERVER_CERT_NEXT_DEVICE_CERT,
+    SERVER_CERT_NEXT_SERVER_CERT,
+    SERVER_CERT_NEXT_END
+};
+
 struct pouch_gateway_attr_handle
 {
     uint16_t value;
@@ -32,37 +39,24 @@ struct pouch_gateway_attr_handle
 struct pouch_gateway_node_info
 {
     struct pouch_gateway_attr_handle attr_handles[POUCH_GATEWAY_GATT_ATTRS];
-    union
-    {
-        struct bt_gatt_discover_params discover_params;
-        struct bt_gatt_read_params read_params;
-        struct bt_gatt_subscribe_params subscribe_params;
-        struct bt_gatt_write_params write_params;
-    };
+    struct bt_gatt_discover_params discover_params;
+    struct bt_gatt_subscribe_params info_subscribe_params;
+    struct bt_gatt_subscribe_params server_cert_subscribe_params;
+    struct bt_gatt_subscribe_params device_cert_subscribe_params;
+    struct bt_gatt_subscribe_params uplink_subscribe_params;
+    struct bt_gatt_subscribe_params downlink_subscribe_params;
     struct pouch_gateway_downlink_context *downlink_ctx;
-    void *downlink_scratch;
-    void *server_cert_scratch;
+    struct pouch_gatt_receiver *info_receiver;
+    struct pouch_gatt_sender *server_cert_sender;
+    struct pouch_gatt_receiver *device_cert_receiver;
+    struct pouch_gatt_sender *downlink_sender;
+    struct pouch_gatt_receiver *uplink_receiver;
     struct pouch_gatt_packetizer *packetizer;
     struct pouch_gateway_uplink *uplink;
     struct pouch_gateway_info_context *info_ctx;
     struct pouch_gateway_device_cert_context *device_cert_ctx;
     struct pouch_gateway_server_cert_context *server_cert_ctx;
+    enum server_cert_next_state server_cert_next;
     bool server_cert_provisioned;
     bool device_cert_provisioned;
 };
-
-/**
- * Allocate a buffer the size of the current MTU for GATT operations.
- * @param conn Bluetooth connection
- * @return Pointer to allocated buffer or NULL on failure
- */
-static inline void *pouch_gateway_bt_gatt_mtu_malloc(struct bt_conn *conn)
-{
-    size_t mtu = bt_gatt_get_mtu(conn);
-    if (mtu < POUCH_GATEWAY_BT_ATT_OVERHEAD)
-    {
-        return NULL;
-    }
-
-    return malloc(mtu - POUCH_GATEWAY_BT_ATT_OVERHEAD);
-}
