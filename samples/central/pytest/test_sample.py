4b9fe2e6398e8b82e2d9e55214c419ffbed51188
Marcin Niestroj <m.niestroj@emb.dev>
2025-12-17 15:43:53 +0100

    samples: drop unused samples (central, lightdb)
    Those samples were used during early development and they are no longer
    used nor maintained. Drop them.
    
    Signed-off-by: Marcin Niestroj <m.niestroj@emb.dev>

--- samples/central/pytest/test_sample.py
+++
@@ -1,9 +0,0 @@
-from twister_harness.device.device_adapter import DeviceAdapter
-
-
-def test_boot(dut: DeviceAdapter):
-    dut.readlines_until('Booting Zephyr')
-
-
-def test_device_found(dut: DeviceAdapter):
-    dut.readlines_until('Device found')
--- samples/lightdb/get/pytest/test_sample.py
+++
@@ -1,9 +0,0 @@
-from twister_harness.device.device_adapter import DeviceAdapter
-
-
-def test_boot(dut: DeviceAdapter):
-    dut.readlines_until('Booting Zephyr')
-
-
-def test_client_connected(dut: DeviceAdapter):
-    dut.readlines_until('Golioth client connected')
